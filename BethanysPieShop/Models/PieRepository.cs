4c3094e5c5e4afe9de6d0567559785db47309ff1
Octavio Martinez <o.martinez.gar@gmail.com>
2019-12-09 15:06:07 +0100

    Create real db repositories for Pies and Categories, change DI to point at those

---
+++ BethanysPieShop/Models/PieRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class PieRepository : IPieRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public PieRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Pie> AllPies => _appDbContext.Pies.Include(p => p.Category);
+
+        public IEnumerable<Pie> PiesOfTheWeek => AllPies.Where(p => p.IsPieOfTheWeek);
+
+        public Pie GetById(int pieId) => AllPies.FirstOrDefault(p => p.PieId == pieId);
+    }
+}
--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -28,8 +28,8 @@ namespace BethanysPieShop
         {
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddScoped<ICategoryRepository, MockCategoryRepository>();
-            services.AddScoped<IPieRepository, MockPieRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IPieRepository, PieRepository>();
 
             services.AddControllersWithViews();
         }
